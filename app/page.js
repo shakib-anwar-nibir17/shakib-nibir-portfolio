25973edfaee17215ef9d35fa1ffe75f569ea7c9d
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-04-24 15:25:48 +0600

    homepage styling complete

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import HomePage from "@/Pages/HomePage";
+import HireMe from "@/components/HireMe";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-light w-full">
       <HomePage />
+      <HireMe />
+      <div className="absolute right-8 bottom-8 inline-block">
+        <Image src="/gears.png" height={200} width={500} alt="gears" />
+      </div>
     </main>
   );
 }
