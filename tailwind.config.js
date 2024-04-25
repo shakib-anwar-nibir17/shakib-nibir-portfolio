01ce84d45cd5fd5c7581ad86a3c19fd8cbea7df0
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-04-25 15:57:20 +0600

    tailwind config chnage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      dark: "#1b1b1b",
-      light: "#f5f5f5",
-      primary: "#B63E96",
-      primaryDark: "#58E6D9",
-    },
-    animation: {
-      "spin-slow": "spin 8s linear infinite",
+    extend: {
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96",
+        primaryDark: "#58E6D9",
+      },
+      animation: {
+        "spin-slow": "spin 8s linear infinite",
+      },
     },
   },
   plugins: [],
